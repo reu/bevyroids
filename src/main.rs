15e14438666aae22a616da78123dfef12b0ddbc9
Rodrigo Navarro <rnavarro1@gmail.com>
2022-06-05 12:28:13 -0300

    Add ufos

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ use collision::{Collidable, CollisionPlugin, CollisionSystemLabel, HitEvent};
 use expiration::{Expiration, ExpirationPlugin};
 use flickering::{Flick, FlickPlugin};
 use physics::{AngularVelocity, Damping, PhysicsPlugin, PhysicsSystemLabel, SpeedLimit, Velocity};
-use rand::Rng;
+use rand::{prelude::SliceRandom, Rng};
 use random::{Random, RandomPlugin};
 
 mod boundary;
@@ -45,7 +45,11 @@ fn main() {
         .add_plugin(RandomPlugin)
         .add_plugin(PhysicsPlugin::with_fixed_time_step(1.0 / 120.0))
         .add_plugin(CollisionPlugin::<Bullet, Asteroid>::new())
+        .add_plugin(CollisionPlugin::<Bullet, Ufo>::new())
+        .add_plugin(CollisionPlugin::<Bullet, Ship>::new())
         .add_plugin(CollisionPlugin::<Asteroid, Ship>::new())
+        .add_plugin(CollisionPlugin::<Asteroid, Ufo>::new())
+        .add_plugin(CollisionPlugin::<Ufo, Ship>::new())
         .add_plugin(BoundaryPlugin)
         .add_plugin(ExpirationPlugin)
         .add_plugin(FlickPlugin)
@@ -62,10 +66,13 @@ fn main() {
         .add_system(thrust_system.after("input"))
         .add_system(asteroid_spawn_system.with_run_criteria(FixedTimestep::step(0.5)))
         .add_system(asteroid_generation_system)
+        .add_system(ufo_spawn_system.with_run_criteria(FixedTimestep::step(1.0)))
         .add_system(explosion_system)
-        .add_system(ship_state_system)
+        .add_system(ship_state_system.before(CollisionSystemLabel))
+        .add_system(ufo_state_system.before(CollisionSystemLabel))
         .add_system(asteroid_hit_system.after(CollisionSystemLabel))
         .add_system(ship_hit_system.after(CollisionSystemLabel))
+        .add_system(ufo_hit_system.after(CollisionSystemLabel))
         .run();
 }
 
@@ -94,10 +101,23 @@ impl ThrustEngine {
 #[derive(Debug, Component, Default)]
 struct SteeringControl(Angle);
 
-#[derive(Debug, Component, Default)]
+#[derive(Debug, Component)]
 struct Weapon {
     cooldown: Timer,
+    force: f32,
     triggered: bool,
+    automatic: bool,
+}
+
+impl Default for Weapon {
+    fn default() -> Self {
+        Self {
+            cooldown: Timer::default(),
+            force: 1000.0,
+            triggered: false,
+            automatic: false,
+        }
+    }
 }
 
 impl Weapon {
@@ -108,6 +128,9 @@ impl Weapon {
         }
     }
 }
+
+#[derive(Debug, Component, Deref)]
+struct WeaponTarget(Entity);
 
 #[derive(Debug, Component, Default)]
 struct Ship {
@@ -149,6 +172,37 @@ impl Default for ShipState {
 
 #[derive(Debug, Component, Default)]
 struct Bullet;
+
+#[derive(Debug, Component, Default)]
+struct Ufo {
+    state: UfoState,
+}
+
+#[derive(Debug)]
+enum UfoState {
+    Alive(Timer),
+    ChangingDirection(Timer),
+}
+
+impl Default for UfoState {
+    fn default() -> Self {
+        UfoState::Alive(Timer::new(Duration::from_secs(5), false))
+    }
+}
+
+impl Ufo {
+    fn alive(duration: Duration) -> Self {
+        Ufo {
+            state: UfoState::Alive(Timer::new(duration, false)),
+        }
+    }
+
+    fn changing_direction(duration: Duration) -> Self {
+        Ufo {
+            state: UfoState::ChangingDirection(Timer::new(duration, false)),
+        }
+    }
+}
 
 #[derive(Debug, Component, Default)]
 struct Explosion;
@@ -206,17 +260,23 @@ fn thrust_system(mut query: Query<(&mut Velocity, &ThrustEngine, &Transform)>) {
 fn weapon_system(
     time: Res<Time>,
     mut commands: Commands,
-    mut query: Query<(&Bounding, &Transform, &mut Weapon)>,
+    mut query: Query<(&Bounding, Option<&WeaponTarget>, &Transform, &mut Weapon)>,
+    targets: Query<&Transform>,
 ) {
-    for (bounds, transform, mut weapon) in query.iter_mut() {
+    for (bounds, target, transform, mut weapon) in query.iter_mut() {
         weapon.cooldown.tick(time.delta());
 
         if weapon.cooldown.finished() && weapon.triggered {
             weapon.triggered = false;
 
-            let bullet_dir = transform.rotation * Vec3::X;
-            let bullet_vel = bullet_dir * 1000.0;
-            let bullet_pos = transform.translation + (bullet_dir * **bounds);
+            let bullet_dir = match target.and_then(|target| targets.get(**target).ok()) {
+                Some(target) => (target.translation - transform.translation).normalize_or_zero(),
+                None => transform.rotation * Vec3::X,
+            };
+
+            let bullet_vel = bullet_dir * weapon.force;
+            let bounds = **bounds + 10.0;
+            let bullet_pos = transform.translation + (bullet_dir * bounds);
 
             commands
                 .spawn_bundle(GeometryBuilder::build_as(
@@ -309,6 +369,136 @@ fn ship_state_system(
                         .remove::<Flick>();
                 }
             }
+        }
+    }
+}
+
+fn ufo_state_system(
+    time: Res<Time>,
+    mut rng: Local<Random>,
+    mut commands: Commands,
+    mut ufos: Query<(Entity, &mut Weapon, &mut Ufo, &mut Velocity, &Transform), With<Ufo>>,
+    ships: Query<(Entity, &Ship), With<Ship>>,
+) {
+    let alive_ships = ships
+        .iter()
+        .filter_map(|(entity, ship)| match ship.state {
+            ShipState::Alive => Some(entity),
+            _ => None,
+        })
+        .collect::<Vec<_>>();
+
+    for (entity, mut weapon, mut ufo, mut velocity, transform) in ufos.iter_mut() {
+        match ufo.state {
+            UfoState::Alive(ref mut timer) => {
+                timer.tick(time.delta());
+
+                if timer.finished() {
+                    *ufo =
+                        Ufo::changing_direction(Duration::from_millis(rng.gen_range(1000..2000)));
+                }
+            }
+
+            UfoState::ChangingDirection(ref mut timer) => {
+                if timer.elapsed().is_zero() {
+                    velocity.y = if transform.translation.y > 0.0 {
+                        -100.0
+                    } else {
+                        100.0
+                    };
+                }
+
+                timer.tick(time.delta());
+
+                if timer.finished() {
+                    velocity.y = 0.0;
+                    *ufo = Ufo::alive(Duration::from_secs(rng.gen_range(4..8)));
+                }
+            }
+        }
+
+        if let Some(ship) = alive_ships.choose(&mut **rng) {
+            weapon.triggered = true;
+            commands.entity(entity).insert(WeaponTarget(*ship));
+        } else {
+            weapon.triggered = false;
+            commands.entity(entity).remove::<WeaponTarget>();
+        }
+    }
+}
+
+fn ufo_spawn_system(
+    window: Res<WindowDescriptor>,
+    mut rng: Local<Random>,
+    mut commands: Commands,
+    ships: Query<Entity, With<Ship>>,
+) {
+    if rng.gen_bool(1.0 / 10.0) {
+        let h = (window.height * 0.8) / 2.0;
+        let w = window.width / 2.0;
+
+        let y = rng.gen_range(-h..h);
+        let x = [-w, w].choose(&mut **rng).copied().unwrap();
+
+        let c = 30.0;
+        let position = Vec3::new(if x > 0.0 { w + c } else { -w - c }, y, 0.0);
+
+        let mut ufo = commands.spawn();
+
+        ufo.insert_bundle(GeometryBuilder::build_as(
+            &shapes::Rectangle {
+                extents: Vec2::new(c, c / 2.0),
+                ..Default::default()
+            },
+            DrawMode::Stroke(StrokeMode::new(Color::BLACK, 1.0)),
+            Transform::default().with_translation(position),
+        ))
+        .insert_bundle(GeometryBuilder::build_as(
+            &{
+                let h = c / 2.5;
+                let w = c;
+                let hw = w / 2.0;
+                let hh = h / 2.0;
+
+                let mut path_builder = PathBuilder::new();
+                path_builder.move_to(Vec2::new(0.0, -hh));
+                path_builder.line_to(Vec2::new(-hw * 0.7, -hh));
+                path_builder.line_to(Vec2::new(-hw, 0.0));
+                path_builder.line_to(Vec2::new(-hw * 0.7, hh));
+                path_builder.line_to(Vec2::new(hw * 0.7, hh));
+                path_builder.line_to(Vec2::new(hw, 0.0));
+                path_builder.line_to(Vec2::new(hw * 0.7, -hh));
+                path_builder.line_to(Vec2::new(0.0, -hh));
+
+                path_builder.move_to(Vec2::new(-hw, 0.0));
+                path_builder.line_to(Vec2::new(hw, 0.0));
+
+                path_builder.move_to(Vec2::new(-hw * 0.5, hh));
+                path_builder.line_to(Vec2::new(-hw * 0.3, hh * 1.8));
+                path_builder.line_to(Vec2::new(hw * 0.3, hh * 1.8));
+                path_builder.line_to(Vec2::new(hw * 0.5, hh));
+
+                path_builder.build()
+            },
+            DrawMode::Stroke(StrokeMode::new(Color::BLACK, 1.0)),
+            Transform::default().with_translation(position),
+        ))
+        .insert(Ufo::alive(Duration::from_secs(rng.gen_range(1..5))))
+        .insert(Weapon {
+            force: rng.gen_range(300.0..500.0),
+            triggered: true,
+            automatic: true,
+            ..Weapon::new(Duration::from_millis(rng.gen_range(1000..3000)))
+        })
+        .insert(Bounding::from_radius(c / 2.0))
+        .insert(Velocity::from(
+            Vec2::new(if x > 0.0 { -1.0 } else { 1.0 }, 0.0) * rng.gen_range(100.0..200.0),
+        ))
+        .insert(Collidable)
+        .insert(BoundaryRemoval);
+
+        if let Some(ship) = ships.iter().collect::<Vec<_>>().choose(&mut **rng) {
+            ufo.insert(WeaponTarget(*ship));
         }
     }
 }
@@ -421,7 +611,12 @@ fn thrust_control_system(keyboard_input: Res<Input<KeyCode>>, mut query: Query<&
 
 fn weapon_control_system(keyboard_input: Res<Input<KeyCode>>, mut query: Query<&mut Weapon>) {
     for mut weapon in query.iter_mut() {
-        weapon.triggered = weapon.triggered || keyboard_input.just_pressed(KeyCode::Space);
+        let pressed = if weapon.automatic {
+            keyboard_input.pressed(KeyCode::Space)
+        } else {
+            keyboard_input.just_pressed(KeyCode::Space)
+        };
+        weapon.triggered = weapon.triggered || pressed;
     }
 }
 
@@ -435,13 +630,19 @@ fn explosion_system(mut query: Query<&mut Transform, With<Explosion>>) {
 
 fn ship_hit_system(
     mut rng: Local<Random>,
-    mut ship_hits: EventReader<HitEvent<Asteroid, Ship>>,
+    mut asteroid_hits: EventReader<HitEvent<Asteroid, Ship>>,
+    mut bullet_hits: EventReader<HitEvent<Bullet, Ship>>,
+    mut ufo_hits: EventReader<HitEvent<Ufo, Ship>>,
     mut commands: Commands,
     query: Query<&Transform, With<Ship>>,
 ) {
-    for hit in ship_hits.iter() {
-        let ship = hit.hurtable();
+    let hits = asteroid_hits
+        .iter()
+        .map(|hit| hit.hurtable())
+        .chain(bullet_hits.iter().map(|hit| hit.hurtable()))
+        .chain(ufo_hits.iter().map(|hit| hit.hurtable()));
 
+    for ship in hits {
         if let Ok(transform) = query.get(ship) {
             for n in 0..12 * 6 {
                 let angle = 2.0 * PI / 12.0 * (n % 12) as f32 + rng.gen_range(0.0..2.0 * PI / 12.0);
@@ -488,19 +689,23 @@ fn asteroid_hit_system(
         if let Ok((transform, radius)) = query.get(asteroid) {
             let position = Vec2::new(transform.translation.x, transform.translation.y);
 
-            if asteroid_sizes.big.contains(radius) {
+            let explosion_size = if asteroid_sizes.big.contains(radius) {
                 let bounds = Bounding::from_radius(rng.gen_range(asteroid_sizes.medium.clone()));
                 asteroid_spawn.send(AsteroidSpawnEvent(position, bounds));
                 asteroid_spawn.send(AsteroidSpawnEvent(position, bounds));
                 asteroid_spawn.send(AsteroidSpawnEvent(position, bounds));
+                5
             } else if asteroid_sizes.medium.contains(radius) {
                 let bounds = Bounding::from_radius(rng.gen_range(asteroid_sizes.small.clone()));
                 asteroid_spawn.send(AsteroidSpawnEvent(position, bounds));
                 asteroid_spawn.send(AsteroidSpawnEvent(position, bounds));
-            }
+                3
+            } else {
+                1
+            };
 
-            for n in 0..12 {
-                let angle = 2.0 * PI / 12.0 * n as f32 + rng.gen_range(0.0..2.0 * PI / 12.0);
+            for n in 0..12 * explosion_size {
+                let angle = 2.0 * PI / 12.0 * (n % 12) as f32 + rng.gen_range(0.0..2.0 * PI / 12.0);
                 let direction = Vec3::new(angle.cos(), angle.sin(), 0.0);
                 let position = direction * rng.gen_range(1.0..20.0) + transform.translation;
 
@@ -521,6 +726,58 @@ fn asteroid_hit_system(
         commands.entity(bullet).despawn();
 
         removed.insert(asteroid);
+        removed.insert(bullet);
+    }
+}
+
+fn ufo_hit_system(
+    mut rng: Local<Random>,
+    mut bullet_hits: EventReader<HitEvent<Bullet, Ufo>>,
+    mut asteroid_hits: EventReader<HitEvent<Asteroid, Ufo>>,
+    mut commands: Commands,
+    query: Query<&Transform, With<Ufo>>,
+) {
+    let mut removed = HashSet::with_capacity(bullet_hits.len() + asteroid_hits.len());
+
+    let hits = bullet_hits
+        .iter()
+        .map(|hit| hit.hurtable())
+        .chain(asteroid_hits.iter().map(|hit| hit.hurtable()));
+
+    for ufo in hits {
+        if removed.contains(&ufo) {
+            continue;
+        }
+
+        if let Ok(transform) = query.get(ufo) {
+            for n in 0..12 * 5 {
+                let angle = 2.0 * PI / 12.0 * (n % 12) as f32 + rng.gen_range(0.0..2.0 * PI / 12.0);
+                let direction = Vec3::new(angle.cos(), angle.sin(), 0.0);
+                let position = direction * rng.gen_range(1.0..20.0) + transform.translation;
+
+                commands
+                    .spawn_bundle(ExplosionBundle::default())
+                    .insert(Transform::default().with_translation(position))
+                    .insert(Velocity::from(
+                        Vec2::new(angle.cos(), angle.sin()) * rng.gen_range(50.0..100.0),
+                    ))
+                    .insert(Expiration::new(Duration::from_millis(
+                        rng.gen_range(400..700),
+                    )))
+                    .insert(Flick::new(Duration::from_millis(rng.gen_range(20..30))));
+            }
+        }
+
+        commands.entity(ufo).despawn();
+        removed.insert(ufo);
+    }
+
+    for hit in bullet_hits.iter() {
+        let bullet = hit.hittable();
+        if removed.contains(&bullet) {
+            continue;
+        }
+        commands.entity(bullet).despawn();
         removed.insert(bullet);
     }
 }
